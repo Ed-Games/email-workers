020be2f27890bf1a43216bddc40f1b1904d87b1a
Ed-Games <edmarcosfilho2015@gmail.com>
2022-07-10 09:23:08 -0300

    update:  getting info from environment variables

--- app/sender.py
+++ app/sender.py
@@ -1,4 +1,5 @@
 import psycopg2
+import os
 import redis
 import json
 from bottle import Bottle, request
@@ -7,11 +8,19 @@ class Sender(Bottle):
 
   def __init__(self):
     super().__init__()
-    self.route('/', method='POST', callback=self.send)
-    self.fila = redis.StrictRedis(host='queue', port=6379, db=0)
+    
+    db_host = os.getenv('DB_HOST', 'db')
+    db_user = os.getenv('DB_USER', 'postgres')
+    db_name = os.getenv('DB_NAME', 'sender')
 
-    DSN = 'dbname=email_sender user=postgres host=db'
-    self.conn = psycopg2.connect(DSN)
+    dsn = f'dbname={db_name} user={db_user} host={db_host}'
+    self.conn = psycopg2.connect(dsn)
+
+    redis_host = os.getenv('REDIS_HOST', 'queue')
+
+    self.fila = redis.StrictRedis(host=redis_host, port=6379, db=0)
+    self.route('/', method='POST', callback=self.send)
+
   
 
   def register_message(self, assunto, mensagem):
